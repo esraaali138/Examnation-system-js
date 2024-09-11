5eb8b4519924b139c7aa452ddc38d4073c2b5255
esraaali138 <esraaali1999138@gmail.com>
2024-09-12 01:32:10 +0300

    update quiz

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,24 @@
-const startingMinutes = 20; 
+const startingMinutes = 20;
 const countdownTimer = document.getElementById("time");
-const totalTime = startingMinutes * 60; 
+const totalTime = startingMinutes * 60;
 let time;
 
 function initializeTimer() {
-  const startTime = localStorage.getItem('timerStart');
+  const startTime = localStorage.getItem("timerStart");
   if (startTime) {
     const startTimeDate = new Date(parseInt(startTime, 10));
     const currentTime = new Date();
-    time = Math.max(0, Math.floor((startTimeDate.getTime() + (totalTime * 1000) - currentTime.getTime()) / 1000));
+    time = Math.max(
+      0,
+      Math.floor(
+        (startTimeDate.getTime() + totalTime * 1000 - currentTime.getTime()) /
+          1000
+      )
+    );
   } else {
     time = totalTime;
-    localStorage.setItem('timerStart', new Date().getTime());
+    localStorage.setItem("timerStart", new Date().getTime());
   }
-
 }
 
 function updateCountdown() {
@@ -22,25 +27,23 @@ function updateCountdown() {
   seconds = seconds < 10 ? "0" + seconds : seconds;
   countdownTimer.innerHTML = `${minutes}:${seconds}`;
 
-
   time--;
 
   if (time < 0) {
     clearInterval(timer);
     updateCounter();
-    localStorage.removeItem('timerStart'); 
+    localStorage.removeItem("timerStart");
     location.replace("timeout.html");
-
   }
 }
 const timer = setInterval(updateCountdown, 1000);
 
-
 function updateCounter() {
   if (counter === 0) {
-        localStorage.setItem("counter", 0);
-      } else {
-        localStorage.setItem("counter", counter);
-      }}
+    localStorage.setItem("counter", 0);
+  } else {
+    localStorage.setItem("counter", counter);
+  }
+}
 
 initializeTimer();
